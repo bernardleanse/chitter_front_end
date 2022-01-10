df7cedef188b265d7832b1ad6e7e9f8bf6408e8e
Bernard Leanse <bernardleanse96@gmail.com>
2022-01-10 15:30:50 +0000

    close button showing

--- src/components/Peep.js
+++ src/components/Peep.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Peep = ({ content }) => {
   return (
     <div className="peep">
+      <img src="https://www.clipartmax.com/png/full/43-434897_thin-close-icon-png.png" className="close"/>
       {content}
     </div>
   )
